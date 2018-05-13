1576660cf1ed750edbe8e2241bf3f3aaae5480c3
Aidan Bane <aidanfbane@gmail.com>
2018-05-13 12:37:13 -0700

    (refactor) Factored components into seperate modules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import About from './components/About';
+import Home from './components/Home';
+import Topics from './components/Topics';
+
 const BasicExample = () => (
   <Router>
     <div>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link href="/" to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link href="/about" to="/about">About</Link>
         </li>
         <li>
-          <Link to="/topics">Topics</Link>
+          <Link href="/topics" to="/topics">Topics</Link>
         </li>
       </ul>
 
@@ -23,48 +27,6 @@ const BasicExample = () => (
       <Route path="/topics" component={Topics} />
     </div>
   </Router>
-);
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
 );
 
 export default BasicExample;
