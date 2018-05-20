ef7df52ac6bce569757a1f875924923477eb33f4
Aidan Bane <aidanfbane@gmail.com>
2018-05-19 19:47:44 -0700

    (cleanup)

--- server/api/collection/index.js
+++
@@ -1,7 +0,0 @@
-const collectionRouter = require('express').Router();
-
-collectionRouter.get('/', function getCollections (req, res) {
-  res.json({collections: 101})
-})
-
-module.exports = collectionRouter
--- server/middleware.js
+++
@@ -1,8 +0,0 @@
-const bodyParser = require('body-parser');
-
-function setGlobalMiddleware (app) {
-  app.use(bodyParser.urlencoded({extended: true}))
-  app.use(bodyParser.json())
-}
-
-module.exports = setGlobalMiddleware
