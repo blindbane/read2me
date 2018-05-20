44cf6ffcba2b7771ce6d17f294303cf6b8e38bf8
Aidan Bane <aidanfbane@gmail.com>
2018-05-19 21:08:27 -0700

    (feat) webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  mode: 'development', 
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, "public")
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public/index.html")
+    })
+  ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [".jsx", ".js", ".json"]
   },
   module: {
     rules: [
