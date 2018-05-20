d871d495a1eabfdc28a48ef21b7e63d746f16784
Aidan Bane <aidanfbane@gmail.com>
2018-05-20 02:27:28 -0700

    (feat) Eslint config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import App from './App';
 import ErrorBoundary from './ErrorBoundary';
 
 ReactDOM.render(
   <ErrorBoundary><App /></ErrorBoundary>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
